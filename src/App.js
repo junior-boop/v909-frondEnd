2da1e60297f256e4786207d74313636db1b7e85d
junior-boop <54283084+junior-boop@users.noreply.github.com>
2020-10-30 21:15:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Helmet>
-          <!-- The core Firebase JS SDK is always required and must be listed first -->
           <script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js"></script>
-
-          <!-- TODO: Add SDKs for Firebase products that you want to use
-               https://firebase.google.com/docs/web/setup#available-libraries -->
           <script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-analytics.js"></script>
 
           <script>
