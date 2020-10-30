5e2ad6513aaddef438712dddea77ba2eb1d8e8b6
junior-boop <54283084+junior-boop@users.noreply.github.com>
2020-10-30 21:12:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Navigation from './navigation/navigation';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <BrowserRouter>
+      <Helmet>
+          <!-- The core Firebase JS SDK is always required and must be listed first -->
+          <script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js"></script>
+
+          <!-- TODO: Add SDKs for Firebase products that you want to use
+               https://firebase.google.com/docs/web/setup#available-libraries -->
+          <script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-analytics.js"></script>
+
+          <script>
+            // Your web app's Firebase configuration
+            // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+            var firebaseConfig = {
+              apiKey: "AIzaSyCOdfML9TeeeCpOGG9sVlKa5nndEkczisw",
+              authDomain: "v909-d9eec.firebaseapp.com",
+              databaseURL: "https://v909-d9eec.firebaseio.com",
+              projectId: "v909-d9eec",
+              storageBucket: "v909-d9eec.appspot.com",
+              messagingSenderId: "670051060419",
+              appId: "1:670051060419:web:083b520402e7c9752ef076",
+              measurementId: "G-BHKJ4GZXQV"
+            };
+            // Initialize Firebase
+            firebase.initializeApp(firebaseConfig);
+            firebase.analytics();
+          </script>
+      </Helmet>
       <Navigation />
     </BrowserRouter>
   );
